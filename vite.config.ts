436bc436c0c868b864b5cb4f9794fb8e99b8a0ab
Jagan6923 <jeyaraman.saravanan23@gmail.com>
2025-02-27 23:39:35 +0530

    Update script source path, import statement, and configure server settings in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "Jagan6923.github.io", 
+  base: "Jagan6923.github.io",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
+  },
+  server: {
+    open: true,
   },
 });
