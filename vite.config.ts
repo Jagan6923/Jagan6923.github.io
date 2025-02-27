aa71e273b40e934d6f89fde0f2bd2eeb922e3eff
Jagan6923 <jeyaraman.saravanan23@gmail.com>
2025-02-27 23:43:30 +0530

    Remove favicon link from index.html and update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "Jagan6923.github.io",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
